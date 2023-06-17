8ba93829639601f5da5c581faac057fe50f7c955
Jérôme Belpaire <jerome.belpaire@gmail.com>
2023-06-17 22:06:50 +0200

    feat: improved home page

--- recipes/views.py
+++ recipes/views.py
@@ -3,9 +3,10 @@ from .models import Recipe, Ingredient
 
 
 def home_view(request):
-    recipes = Recipe.objects.order_by("-created_on")[:5]
+    recipes = Recipe.objects.order_by("-created_on")[:9]
     context = {"recipes": recipes}
     return render(request, "recipes/home.html", context)
+
 
 def recipe_view(request, recipe_slug, guests=1):
     recipe = get_object_or_404(Recipe, slug=recipe_slug)
