cdc03b62d4b6cb03881f255aeb96253b3126e6d6
Jérôme Belpaire <jerome.belpaire@gmail.com>
2023-06-17 21:32:44 +0200

    feat: add homepage

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("home/", views.home_view, name="home_view"),
+    path("recipe/<slug:recipe_slug>/", views.recipe_view, name="recipe_uncounted_view"),
     path("recipe/<slug:recipe_slug>/<int:guests>/", views.recipe_view, name="recipe_view"),
-    path("recipe/<slug:recipe_slug>/", views.recipe_view, name="recipe_view"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Recipe, Ingredient
 
 
+def home_view(request):
+    recipes = Recipe.objects.order_by("-created_on")[:5]
+    context = {"recipes": recipes}
+    return render(request, "recipes/home.html", context)
+
 def recipe_view(request, recipe_slug, guests=1):
     recipe = get_object_or_404(Recipe, slug=recipe_slug)
     ingredients = Ingredient.objects.filter(recipe=recipe)
