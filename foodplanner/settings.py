2713ef8b4fcfd2000a3f20561a89a52960ab3d22
Jérôme Belpaire <jerome.belpaire@gmail.com>
2023-07-19 22:15:00 +0200

    feat: temp settings file

--- foodplanner/settings.py
+++ foodplanner/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = "dlksjolcjojqjoisqjcqkljskjdcqlkjsdfsdqsdqsdh"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "[::1]", "foodplanner101.azurewebsites.net"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "foodplanner101.azurewebsites.net"]
 
 
 # Application definition
