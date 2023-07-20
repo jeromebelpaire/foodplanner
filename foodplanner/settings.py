ac54c8c9e5c4973baefac65350f6ef2d32a4058d
Jérôme Belpaire <jerome.belpaire@gmail.com>
2023-07-20 08:44:50 +0200

    feat: remove website 102

--- foodplanner/settings.py
+++ foodplanner/settings.py
@@ -33,12 +33,10 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "[::1]",
     "foodplanner101.azurewebsites.net",
-    "foodplanner102.azurewebsites.net",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://foodplanner101.azurewebsites.net",
-    "https://foodplanner102.azurewebsites.net",
 ]
 
 SECURE_SSL_REDIRECT = False
