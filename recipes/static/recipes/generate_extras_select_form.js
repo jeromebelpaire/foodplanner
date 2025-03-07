7a29f4496c899437d9e5c95d632a127d2110bce3
Jérôme Belpaire <jerome.belpaire@gmail.com>
2025-03-07 12:32:18 +0100

    refactor: remove jquery

--- recipes/static/recipes/generate_extras_select_form.js
+++ recipes/static/recipes/generate_extras_select_form.js
@@ -1,15 +1,22 @@
 function generate_extras_select_form(data) {
-  // Update the recipe form HTML
-  $("#extras-select-form").html(data.extras_form_html);
-  $("#id_extras").change(function () {
-    var selectedExtra = $(this).children("option:selected").length;
-    $("#quantity").empty();
+  document.getElementById("extras-select-form").innerHTML = data.extras_form_html;
+  
+  const extras = document.getElementById("id_extras");
+  extras.addEventListener("change", function () {
+
+    const selectedExtra = this.selectedOptions.length;
+    const quantity = document.getElementById('quantity')
+
+    quantity.innerHTML = "";
+
     for (i = 0; i < selectedExtra; i++) {
-      $("#quantity").append(
-        '<input type="number" name="quantity" min="1" class="form-control my-2" placeholder="Quantity of extra ' +
-          (i + 1) +
-          '">'
-      );
-    }
-  });
+      const input = document.createElement("input");
+      input.type = "number";
+      input.name = "quantity";
+      input.min = "1";
+      input.className = "form-control my-2";
+      input.placeholder = "Quantity of extra " + (i + 1);
+      quantity.appendChild(input);
+      }
+  })
 }
--- recipes/static/recipes/generate_recipe_select_form.js
+++ recipes/static/recipes/generate_recipe_select_form.js
@@ -1,15 +1,22 @@
 function generate_recipe_select_form(data) {
-  // Update the recipe form HTML
-  $("#recipe-select-form").html(data.recipe_form_html);
-  $("#id_recipes").change(function () {
-    var selectedRecipes = $(this).children("option:selected").length;
-    $("#guests").empty();
-    for (i = 0; i < selectedRecipes; i++) {
-      $("#guests").append(
-        '<input type="number" name="guests" min="1" class="form-control my-2" placeholder="Number of guests for recipe ' +
-          (i + 1) +
-          '">'
-      );
+  document.getElementById("recipe-select-form").innerHTML = data.recipe_form_html;
+
+  const idRecipesEl = document.getElementById("id_recipes");
+  idRecipesEl.addEventListener("change", function () {
+    // Get the count of selected options (works for a multi-select)
+    const selectedRecipes = this.selectedOptions.length;
+    const guestsEl = document.getElementById("guests");
+
+    guestsEl.innerHTML = "";
+
+    for (let i = 0; i < selectedRecipes; i++) {
+      const input = document.createElement("input");
+      input.type = "number";
+      input.name = "guests";
+      input.min = "1";
+      input.className = "form-control my-2";
+      input.placeholder = "Number of guests for recipe " + (i + 1);
+      guestsEl.appendChild(input);
     }
   });
 }
