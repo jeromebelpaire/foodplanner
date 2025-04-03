43e95ad5855fb3d4856f30d511dc48616fd280b0
Jérôme Belpaire <jerome.belpaire@gmail.com>
2025-04-03 16:27:59 +0200

    feat: switch db to azure

--- foodplanner/settings.py
+++ foodplanner/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = "foodplanner.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("DBNAME"),
-#         "HOST": os.getenv("DBHOST"),
-#         "USER": os.getenv("DBUSER"),
-#         "PASSWORD": os.getenv("DBPASS"),
-#         "OPTIONS": {"sslmode": "require"},
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DBNAME"),
+        "HOST": os.getenv("DBHOST"),
+        "USER": os.getenv("DBUSER"),
+        "PASSWORD": os.getenv("DBPASS"),
+        "OPTIONS": {"sslmode": "require"},
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
